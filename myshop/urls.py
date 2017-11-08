a60025c44733fafad0a3d69d71c6415d51b118fb
team1 <groyce@unomaha.edu>
2017-11-08 14:01:46 -0600

    My shop, first commit

---
+++ myshop/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('shop.urls', namespace='shop')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
